0b10b6b46f7839d62abee8303cecbfae381a29a3
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-18 13:48:05 +0100

    exercice 2 session java

---
+++ main.js
@@ -0,0 +1,18 @@
+//function changer_style()
+//{
+    //var p = document.getElementById('montexte');
+    //p.style.color="white";
+    //p.style.backgroundColor="#bbb";
+   // p.style.border="1px solid gray";
+   // p.style.padding="5px";
+
+ // }
+
+
+
+ function changer_style(){
+
+    var p = document.getElementById('montexte');
+    p.classList.add("active");
+
+  }
